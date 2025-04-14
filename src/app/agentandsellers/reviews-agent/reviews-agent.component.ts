6ae050d6d9559df94f1f224241c1b01a79936e36
Mennajihad <151662396+Mennajihad@users.noreply.github.com>
2025-04-15 02:39:00 +0300

    agent review

--- src/app/agentandsellers/reviews-agent/reviews-agent.component.ts
+++ src/app/agentandsellers/reviews-agent/reviews-agent.component.ts
@@ -13,32 +13,35 @@ export class ReviewsAgentComponent {
       date: '17 Aug, 23',
       rating: 4.7,
       comment: 'Lorem ipsum dolor sit amet consectetur. Pellentesque sed nulla facilisi diam posuere aliquam suscipit quam.',
-      image: 'assets/users/user1.jpg',
-      images: ['assets/review1.jpg', 'assets/review2.jpg', 'assets/review3.jpg']
+      image: 'assets/img/profile3.png',
+      images: ['assets/img/regions/house.png', 'assets/img/regions/house.png', 'assets/img/regions/house.png']
+
     },
     {
       name: 'Rashed Kabir',
       date: '13 Jun, 23',
       rating: 4.9,
-      comment: 'Lorem ipsum dolor sit amet consectetur. Pellentesque sed nulla facilisi diam posuere aliquam suscipit quam.',
-      image: 'assets/users/user2.jpg',
-      images: ['assets/review4.jpg', 'assets/review5.jpg', 'assets/review6.jpg', 'assets/review7.jpg', 'assets/review8.jpg']
+      comment: ' my house dolor sit amet consectetur. Pellentesque sed nulla facilisi diam posuere aliquam suscipit quam.',
+      image: 'assets/img/profile2.png',
+      images: ['assets/img/regions/house.png', 'assets/img/regions/house.png', 'assets/img/regions/house.png', 'assets/img/regions/house.png']
     },
     {
       name: 'Perty Jinta',
       date: '17 Aug, 23',
       rating: 4.7,
       comment: 'Amet amet id cursus dignissim. Eget vitae amet tempus sit mattis. Integer semper condimentum nunc augue aliquet quam.',
-      image: 'assets/users/user3.jpg',
-      images: []
+      image: 'assets/img/profile2.png',
+      images: ['assets/img/regions/house.png','assets/img/regions/house.png']
     },
     {
       name: 'Milon Ahmed',
       date: '07 Jun, 23',
       rating: 4.7,
       comment: 'Pellentesque sed nulla facilisi diam posuere aliquam suscipit quam.',
-      image: 'assets/users/user4.jpg',
-      images: []
+      image: 'assets/img/profile3.png',
+      images: ['assets/img/regions/house.png','assets/img/regions/house.png']
     }
   ];
+
+
 }
