7b783af037ef5f8981f2ca074c28bd683a229425
Mennajihad <151662396+Mennajihad@users.noreply.github.com>
2025-04-15 12:23:50 +0300

    notifaction agent

--- src/app/agentandsellers/notification-agent/notification-agent.component.ts
+++ src/app/agentandsellers/notification-agent/notification-agent.component.ts
@@ -1,11 +1,63 @@
 import { Component } from '@angular/core';
+import {NgClass, NgForOf} from '@angular/common';
 
 @Component({
-  selector: 'app-notification-agent',
-  standalone: false,
+  selector: 'app-notification-admin',
   templateUrl: './notification-agent.component.html',
-  styleUrl: './notification-agent.component.css'
+  imports: [
+    NgClass,
+    NgForOf
+  ],
+  styleUrls: ['./notification-agent.component.css'] // تأكد من أنها بصيغة "styleUrls" وليس "styleUrl"
 })
 export class NotificationAgentComponent {
+
+  notifications = [
+    {
+      type: 'info',
+      icon: 'bi-envelope-fill',
+      title: 'New advertisement',
+      message: 'User ahmad@gmail.com sent a message about "Apartment #A12".',
+      time: 'Just now'
+    },
+    {
+      type: 'warning',
+      icon: 'bi-tag-fill',
+      title: 'Price Drop',
+      message: 'Wafa adham replay your massage.',
+      time: '10 mins ago'
+    },
+    {
+      type: 'success',
+      icon: 'bi-calendar-check-fill',
+      title: 'Booking Confirmed',
+      message: 'Booking for Apartment #12B is confirmed by Nour Khaled.',
+      time: '1 hour ago'
+    },
+    {
+      type: 'danger',
+      icon: 'bi-x-octagon-fill',
+      title: 'Payment Failed',
+      message: 'Transaction for property #2013 failed. Please check payment gateway.',
+      time: '2 hours ago'
+    }
+  ];
+
+
+  selectedNotification: any = null;
+
+
+  openModal(notification: any) {
+    this.selectedNotification = notification;
+
+    const modalElement = document.getElementById('notificationModal');
+    if (modalElement) {
+      const modal = new (window as any).bootstrap.Modal(modalElement);
+      modal.show();
+    }
+  }
+  removeReview(review: any) {
+    this.notifications = this.notifications.filter(r => r !== review);
+  }
 
 }
